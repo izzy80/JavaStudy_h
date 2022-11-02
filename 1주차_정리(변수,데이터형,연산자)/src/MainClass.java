1de55c9fbbb25e45c1b6261ff64e990022e719ee
izzy80 <jiheon809@gmail.com>
2022-11-02 21:39:00 +0900

    1111

---
+++ 1주차_정리(변수,데이터형,연산자)/src/MainClass.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		FileReader fr = new FileReader("C:\\javaDev\\news.txt");
+//		
+//		int i = 0;
+//		String s="";
+//		while((i=fr.read())!=-1)
+//		{
+//			s+=String.valueOf((char)i);
+//			
+//		}
+//		fr.close();
+//		System.out.println(s);
+
+	}
+
+}
